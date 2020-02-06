a39c4fb09473e8572fbbe9f32ba7c345d9a0adaf
2ndyrGroupC <2ndyrGroupC@pn.local>
2020-02-06 11:32:32 +0800

    Initial

--- app/Http/Controllers/TimeTrackerController.php
+++ app/Http/Controllers/TimeTrackerController.php
@@ -15,22 +15,4 @@ class TimeTrackerController extends Controller
            $name = DB::select('select * from users');
            return view('landing',['name'=>$name]);
         }
-
-        public function index()
-        {
-            return TimeTracker::mine()->with('timers')->get()->toArray();
-        }
-
-        public function store(Request $request)
-        {
-            // returns validated data as array
-            $data = $request->validate(['name' => 'required|between:2,20']);
-
-            // merge with the current user ID
-            $data = array_merge($data, ['user_id' => auth()->user()->id]);
-
-            $project = TimeTracker::create($data);
-
-            return $project ? array_merge($project->toArray(), ['timers' => []]) : false;
-        }
 }
